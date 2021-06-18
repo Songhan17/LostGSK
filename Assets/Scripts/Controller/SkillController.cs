e31b41587b50ce47a3139a685cc116726c08ab6d
Songhan17 <839924598@qq.com>
2021-06-18 16:16:36 +0800

    技能相关

---
+++ Assets/Scripts/Controller/SkillController.cs
@@ -0,0 +1,33 @@
+﻿
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SkillController : MonoBehaviour
+{
+
+    private volatile List<Skill> skillList;
+
+    // 获取技能列表
+    public List<Skill> GetList(Skill.SkillType type)
+    {
+        List<Skill> skills = new List<Skill>();
+        skillList.ForEach(e =>
+        {
+            if (e.Type == type)
+            {
+                skills.Add(e);
+            }
+        });
+        return skills;
+    }
+
+    // 获得技能
+    public void GetSkill(int id)
+    {
+        skillList.Add(SkillManager.Instance.GetSkillById(id));
+    }
+
+
+
+}
